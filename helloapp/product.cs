ddd1fc049eb1b045e84bccc139d554f7e13f4cdf
sejalkulkarni9083 <147849986+sejalkulkarni9083@users.noreply.github.com>
2025-02-20 13:45:47 +0530

    csharp files

---
+++ helloapp/product.cs
@@ -0,0 +1,10 @@
+namespace Catalog  //Module name
+{
+    public class Product  //blueprint
+    {
+        public int Id { get; set; }         //properties
+        public string Title { get; set; }   //properties
+        public string Description { get; set; }     
+        public decimal Price { get; set; }
+    }
+}
