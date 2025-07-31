4734d9fdcf8b289899ac7c8f752a9eb96fb18ccf
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-07-31 06:10:33 -0700

    sample.cs deleted

--- MiniProject/TFLEcommerce/obj/Debug/net9.0/dotnetproject.AssemblyInfo.cs
+++ MiniProject/TFLEcommerce/obj/Debug/net9.0/dotnetproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11db18271ee155f70a96797648eba84f757ff505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fb3694304507bf68192bcd8a0209d33bf9ca18")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
