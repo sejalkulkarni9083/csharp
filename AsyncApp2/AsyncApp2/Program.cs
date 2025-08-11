57cc81252302b63c4a528b19bb27f0fa99064c30
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-08-11 10:19:02 -0700

    AsyncApp2 added

---
+++ AsyncApp2/AsyncApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using AsyncApp2.Helper;
+
+
+Console.WriteLine("Primary Thread:" + Thread.CurrentThread.ManagedThreadId.ToString());
+
+//Asynchronous method
+//c# 2.0
+//robustness
+
+//task 1:
+Action sayWelcome = delegate ()
+{
+
+    int count = 786;
+
+    count++;
+    Console.WriteLine("Thread ID:" + Thread.CurrentThread.ManagedThreadId.ToString());
+    Console.WriteLine($"Count is {count}");
+    Console.WriteLine("Welcome to C#");
+
+};
+
+
+
+//concise syntax
+//task 2:
+Action sayHello = () =>
+{
+    int count = 745;
+    count++;
+    Console.WriteLine("Thread ID:" + Thread.CurrentThread.ManagedThreadId.ToString());
+    Console.WriteLine($"Count is {count}");
+    Console.WriteLine("Hello World");
+};
+
+
+//blocking call
+sayWelcome();
+sayHello();
+
+//define a task
+
+//non-blocking call
+
+Task.Run(static () =>
+{
+    Console.WriteLine("Thread ID:" + Thread.CurrentThread.ManagedThreadId.ToString());
+    Console.WriteLine("Task is running");
+    //Tax clculation Logic
+    decimal amount = 500000;
+    //invoke the delegate
+    TaxCalculationDelegate incomeTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateIncomeTax);
+    TaxCalculationDelegate proxyTaxCalculator = incomeTaxCalculation;
+
+    decimal tax = proxyTaxCalculator(amount);
+});
+
+Task task1 = Task.Run(() =>
+{
+    Console.WriteLine("Thread ID : " + Thread.CurrentThread.ManagedThreadId.ToString());
+    Console.WriteLine("Welcome to c#");
+});
