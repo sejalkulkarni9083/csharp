d7bd8f6e7da79cccd98589cb62df59893c8456fb
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-08-11 10:11:39 -0700

    AsyncApp ject added

---
+++ AsyncApp/AsyncApp/Helper/TaxManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncApp.Helper
+{
+    public delegate decimal TaxCalculationDelegate(decimal amount);
+    public class TaxManager
+    {
+        public static decimal CalculateIncomeTax(decimal income)
+        {
+            decimal taxRate = 0.2m;
+            return income * taxRate;
+        }
+        public static decimal CalculateSalesTax(decimal amount)
+        {
+            decimal taxRate = 0.07m; // 7% sales tax
+            return amount * taxRate;
+        }
+        public static decimal CalculatePropertyTax(decimal propertyValue)
+        {
+            decimal taxRate = 0.015m; // 1.5% property tax
+            return propertyValue * taxRate;
+        }
+        public static decimal CalculateCapitalGainTax(decimal capitalGains)
+        {
+            decimal capitalGainTaxRate = 0.15m;
+            return capitalGains * capitalGainTaxRate;
+
+        }
+        public static decimal CalculateCorporateTax(decimal corporateIncome)
+        {
+            decimal corporateTaxRate = 0.25m;
+            return corporateIncome * corporateTaxRate;
+        }
+
+        public static decimal CalculateWealthTax(decimal netWorth)
+        {
+            decimal wealthTaxRate = 0.01m;
+            return netWorth * wealthTaxRate;
+        }
+    }
+}
---
+++ AsyncApp/AsyncApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AsyncApp.Helper;
+//direct call
+/*
+decimal incomeTax = TaxManager.CalculateIncomeTax(150000);
+decimal salesTax = TaxManager.CalculateSalesTax(1000);
+decimal propertyTax = TaxManager.CalculatePropertyTax(300000);
+Console.WriteLine($"Income Tax: {incomeTax:C}");
+Console.WriteLine($"Sales Tax: {salesTax:C}");
+Console. WriteLine($"Property Tax: {propertyTax:C}");
+*/
+
+
+
+//indirect call
+
+//create instance of delegate
+// attaching methods to the delegate
+
+TaxCalculationDelegate incometaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateIncomeTax);
+TaxCalculationDelegate salesTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateSalesTax);
+TaxCalculationDelegate propertyTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculatePropertyTax);
+
+TaxCalculationDelegate capitalGainsTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateCapitalGainTax);
+TaxCalculationDelegate corporateTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateCorporateTax);
+TaxCalculationDelegate wealthTaxCalculation = new TaxCalculationDelegate(TaxManager.CalculateWealthTax);
+//invoke delegate
+decimal amount = 500000;
+
+//invoke delegate
+//dynamically invoke the delegate
+
+TaxCalculationDelegate proxyTaxCalculator = incometaxCalculation;
+//(+=) multicast delegate
+//attach Additional delegates to proxyTaxcalculator
+TaxCalculationDelegate += salesTaxCalculation;
+TaxCalculationDelegate += propertyTaxCalculation;
+TaxCalculationDelegate += capitalGainsTaxCalculation;
+TaxCalculationDelegate += corporateTaxCalculation;
+TaxCalculationDelegate += wealthTaxCalculation;
+// establishing chain of delegates
+
+
+//invoke all delegates
+//initiate a chain reaction by invoking a proxy delegate
+
+//proxyTaxCalculator.DynamicInvoke(amount);
+
+//Asynchronous invocation
+IAsyncResult asyncResult = proxyTaxCalculator.BeginInvoke(amount, null, null);
+
+//wait for the result
+asyncResult.AsyncWaitHandle.WaitOne();
+
+//get the result
+decimal result = proxyTaxCalculator.EndInvoke(asyncResult);
+Console.WriteLine($"Total Tax: {result}");
