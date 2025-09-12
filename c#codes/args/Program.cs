845c36cbb9c35563fbd98fae75fbdb1c83f38152
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-12 02:38:34 -0700

    arguments folder added

---
+++ c#codes/args/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+ 
+
+ 
+public class HelloWorld 
+{ 
+          public static void Main (string [] args) 
+          { 
+                Console.WriteLine ("parameter count = {0}", args.Length); 
+                Console.WriteLine ("Hello sanika {0}", args.Length); 
+                Console.ReadLine (); 
+ 
+          } 
+} 
