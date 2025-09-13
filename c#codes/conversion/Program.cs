0f7407694d5114270305bbf55a8efcabce77eef5
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-13 09:19:11 -0700

    conversion folder updated

--- c#codes/conversion/Program.cs
+++ c#codes/conversion/Program.cs
@@ -4,12 +4,12 @@ class TypeCastingExample
 {
     static void Main()
     {
-        int x = 543454;
+        int x = 543454;   
 
-        long y = x;
+        long y = x;        // Implicit conversion
 
        
-        short z = (short)x;
+        short z = (short)x;  // Explicit conversion
 
         Console.WriteLine("Original int value: " + x);
         Console.WriteLine("Converted to long: " + y);
