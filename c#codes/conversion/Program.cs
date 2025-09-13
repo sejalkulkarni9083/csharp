e4c3df8df40121c3d13e34b100fb5b4458ea3bf0
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-13 08:38:25 -0700

    conversion folder updated

--- c#codes/conversion/Program.cs
+++ c#codes/conversion/Program.cs
@@ -3,8 +3,8 @@
 public class Conversion
 {
     int x = 543454;
-    long y = x;
-    short z = (short)x;
+    long y = x;          //implicit conversion 
+    short z = (short)x;     //explicit conversion                   
     public static void Main(string[] args)
     {
         Console.WrirteLlin(y);
