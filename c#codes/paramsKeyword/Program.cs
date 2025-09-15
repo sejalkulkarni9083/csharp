6a48d654d6a738b7ac495c0969934491d1817536
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-15 10:43:47 -0700

    code added

--- c#codes/paramsKeyword/Program.cs
+++ c#codes/paramsKeyword/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void ViewNames(params string[] names)
+    {
+        Console.WriteLine("Names: {0}, {1}, {2}", names[0], names[1], names[2]);
+    }
+
+    public static void Main(string[] args)
+    {
+        ViewNames("Nitin", "Nilesh", "Shrinivas");
+    }
+}
