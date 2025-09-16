f211cd5b71306a58aaaa471d2ee6cba4798c0f66
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-16 11:04:25 -0700

    new code

--- c#codes/paramsKeyword/Program.cs
+++ c#codes/paramsKeyword/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void ViewNames(params string[] names)
     {
         Console.WriteLine("Names: {0}, {1}, {2}", names[0], names[1], names[2]);
-        Console.WriteLine("Names: {0}, {1}, {2}", names[0], names[1], names[2]);
+    
     }
 
     public static void Main(string[] args)
--- c#codes/paramsKeyword/obj/Debug/net9.0/paramsKeyword.AssemblyInfo.cs
+++ c#codes/paramsKeyword/obj/Debug/net9.0/paramsKeyword.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("paramsKeyword")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722096d55b680cd4bba9287447888f738ceacdbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f488f39255fc869a7f2ba07059f8f86124a86129")]
 [assembly: System.Reflection.AssemblyProductAttribute("paramsKeyword")]
 [assembly: System.Reflection.AssemblyTitleAttribute("paramsKeyword")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
