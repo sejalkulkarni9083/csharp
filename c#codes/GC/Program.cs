1702c181d6f14a39e9f5be1ae1efc72d5e7c07f5
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-20 09:59:45 -0700

    gc sample code

--- c#codes/GC/Program.cs
+++ c#codes/GC/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+
+class MyResource : IDisposable
+{
+    public MyResource()
+    {
+        Console.WriteLine("MyResource created.");
+    }
+
+    public void Use()
+    {
+        Console.WriteLine("Using MyResource...");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("MyResource disposed.");
+    }
+
+    ~MyResource()
+    {
+        Console.WriteLine("MyResource finalized.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyResource resource = new MyResource();
+        resource.Use();
+        resource.Dispose(); // Manually calling Dispose
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("Done.");
+    }
+}
