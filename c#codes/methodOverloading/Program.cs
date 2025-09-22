952662a252155febeedffc7043daf1d214c73960
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-22 09:41:50 -0700

    added to methodOverloading folder

--- c#codes/methodOverloading/Program.cs
+++ c#codes/methodOverloading/Program.cs
@@ -2,29 +2,24 @@
 
 class Calculator
 {
-    // Method with 2 int parameters
+    //method Overloading
     public int Add(int a, int b)
     {
         return a + b;
     }
 
-    // Overloaded method with 3 int parameters
     public int Add(int a, int b, int c)
     {
         return a + b + c;
     }
 
-    // Overloaded method with 2 double parameters
+    
     public double Add(double a, double b)
     {
         return a + b;
     }
 
-    // Overloaded method with 1 int and 1 double parameter
-    public double Add(int a, double b)
-    {
-        return a + b;
-    }
+   
 }
 
 class Program
@@ -33,9 +28,8 @@ class Program
     {
         Calculator calc = new Calculator();
 
-        Console.WriteLine(calc.Add(5, 10));           // Output: 15
-        Console.WriteLine(calc.Add(1, 2, 3));         // Output: 6
-        Console.WriteLine(calc.Add(2.5, 3.5));        // Output: 6.0
-        Console.WriteLine(calc.Add(4, 5.5));          // Output: 9.5
+        Console.WriteLine(calc.Add(5, 10));           
+        Console.WriteLine(calc.Add(1, 2, 3));       
+        Console.WriteLine(calc.Add(2.5, 3.5));    
     }
 }
