45c3b930d6a5fb39e5980724ca6b3b1d8ce8aca1
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-26 11:00:05 -0700

    polymorphism code added

--- c#codes/polmmmorphism/Program.cs
+++ c#codes/polmmmorphism/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("The animal makes a sound.");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The dog barks.");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The cat meows.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.Speak();  
+        myDog.Speak();    
+        myCat.Speak();    
+    }
+}
