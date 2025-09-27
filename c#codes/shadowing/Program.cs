a83959a93acd620bd972bd771b8ef2c0458ab044
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-27 08:49:19 -0700

    code added

--- c#codes/shadowing/Program.cs
+++ c#codes/shadowing/Program.cs
@@ -1,16 +1,20 @@
 ﻿
+//shadowing : hides the base class member
 
 using System;
 
 class Employee
 {
+    public int basic_sal=1000;
     public virtual double CalculateSalary()
     {
         return basic_sal;
     }
 } 
 class SalesEmployee:Employee 
-{ double sales, comm; 
+{
+    double sales = 20000;
+    double comm = 0.1; 
   public new double CalculateSalary () 
      {return basic_sal+ (sales * comm) ;} 
 }
