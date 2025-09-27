5ec523d07ff20fdafa8e6cc9247717d586ab4b88
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-27 08:38:15 -0700

    code added

--- c#codes/shadowing/Program.cs
+++ c#codes/shadowing/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+
+class Employee
+{
+    public virtual double CalculateSalary()
+    {
+        return basic_sal;
+    }
+} 
+class SalesEmployee:Employee 
+{ double sales, comm; 
+  public new double CalculateSalary () 
+     {return basic_sal+ (sales * comm) ;} 
+}
+
+class Program
+{
+    static void Main()
+    {
+        SalesEmployee sper = new SalesEmployee();
+        Double salary = sper.CalculateSalary();
+        Console.WriteLine(salary);
+    }
+}
