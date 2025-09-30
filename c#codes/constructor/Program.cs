85ee017294f204c0e3f7731339317a8f341f46cf
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-09-30 11:02:15 -0700

    constructor code added

--- c#codes/constructor/Program.cs
+++ c#codes/constructor/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Person
+{
+   
+    public string Name;
+    public int Age;
+
+    
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+   
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+     
+        Person person1 = new Person("Alice", 30);
+        person1.DisplayInfo(); 
+    }
+}
