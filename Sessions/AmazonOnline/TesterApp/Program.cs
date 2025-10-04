357e337877ccdb073de0097c3ea6b2c9d994dbc9
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-10-04 11:03:15 -0700

    TesterApp project Added

---
+++ Sessions/AmazonOnline/TesterApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace TesterApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to dotnet programmng.");
+            int count = 45;
+            count = count++;
+            if(count <= 30)
+            {
+                while(count < 299)
+                {
+                    Console.WriteLine("Count = {0}", count);
+                }
+            }
+            Console.WriteLine("Please enter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Goodmorning {0}", name);
+            Console.ReadLine();
+
+        }
+    }
+}
