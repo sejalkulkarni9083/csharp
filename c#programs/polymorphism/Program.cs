930648a417f34d682e312098027a2237204dfb6e
sejal kulkarni <sejalkulkarni017@gmail.com>
2025-12-07 22:14:04 +0530

    polymorphism

--- c#programs/polymorphism/Program.cs
+++ c#programs/polymorphism/Program.cs
@@ -1,1 +1,30 @@
-﻿using system;
+﻿using System;
+
+public abstract class PaymentMethod
+{
+    public abstract void ProcessPayment(decimal amount);
+}
+
+public class CreditCardPayment : PaymentMethod
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing credit card payment of ${amount}");
+    }
+}
+
+public class PayPalPayment : PaymentMethod
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing PayPal payment of ${amount}");
+    }
+}
+
+public class BankTransferPayment : PaymentMethod
+{
+    public override void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing bank transfer payment of ${amount}");
+    }
+}
