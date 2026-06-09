b410939cade42d99720b1acd73c931ae973fc3d3
sejalkulkarni9083 <sejalkulkarni017@gmail.com>
2026-06-09 17:44:13 +0530

    DelegateProject added

--- DelegateApp/Program.cs
+++ DelegateApp/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Notification
+{
+
+    
+    public class Program{
+
+    public delegate void OrderOperation(int orderId);
+
+
+    public static void ProcessOrder(int orderId)
+    {
+        Console.WriteLine("Order is processed");
+    }
+
+    public static void CancelOrder(int orderId)
+    {
+        Console.WriteLine("Order is canceled");
+    }
+
+     public static void ShipProduct(int orderId)
+    {
+        Console.WriteLine("product is deliverd to customer end");
+    }
+
+    public static void Main(string [] args){
+
+
+
+        OrderOperation o1=new OrderOperation(ProcessOrder); 
+        OrderOperation o2=new OrderOperation(CancelOrder);  
+        OrderOperation o3=new OrderOperation(ShipProduct); 
+        
+        o3.Invoke(98);  
+
+
+
+        OrderOperation masterO=null;
+
+
+        masterO+=o1;
+        masterO+=o2;
+        masterO+=o3;
+        
+        masterO(876);
+        Console.WriteLine("After detachment");
+
+        masterO-=o3;
+        masterO(876);
+
+    }
+    }
+}
