ff786c86664098511bc899e98fc0ca8989749196
sejalkulkarni9083 <sejalkulkarni017@gmail.com>
2026-06-11 16:45:32 +0530

    InsuranceAPI project added

---
+++ InsuranceAPI/Models/Claim.cs
@@ -0,0 +1,1 @@
+namespace InsuranceAPI.Models;
---
+++ InsuranceAPI/Models/Policy.cs
@@ -0,0 +1,11 @@
+
+namespace InsuranceAPI.Models;
+
+
+public class Policy
+{
+    public string PolicyNumber { get; set; }
+    public string CustomerName { get; set; }
+    public string PolicyType { get; set; }
+    public decimal PremiumAmount { get; set; }
+}
