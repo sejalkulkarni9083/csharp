46e62e0e944d9d99586b6f148f53ab626cb64b91
sejalkulkarni9083 <sejalkulkarni017@gmail.com>
2026-06-29 14:47:44 +0530

    arrays project added

---
+++ Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int[,] arr1 ={
+                {1, 2, 3},
+                {4, 5, 6}
+              };
+
+int[,] arr2 ={
+                {7, 8, 9},
+                {10, 11, 12}
+             };
+            
+
+int [,]result=new int[2,3];
+for(int i=0;i<2;i++)
+{
+    for(int j = 0; j <3; j++)
+    {
+        result[i,j]=arr1[i,j]+arr2[i,j];
+    }
+}
+
+for(int i = 0; i <3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(result[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
+
+
+      
+    
